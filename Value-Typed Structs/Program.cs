3f198f6aeecf20aca50cfce0cc10d59380cc5213
Mohamed Shahin <ms4211913@gmail.com>
2024-08-05 21:32:11 +0300

    Commit Record 3 Project files to Local Repo

---
+++ Value-Typed Structs/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var p1 = new point(7, 8);
+        var p2 = new point(7, 8);
+        Console.WriteLine($"is p1 = p2 : {p1.Equals(p2)}");
+        
+        Console.WriteLine($"is p1 points to p2 : {Object.ReferenceEquals(p1,p2)}");
+
+        Console.WriteLine($"p1 Hash :{p1.GetHashCode()}" );
+        Console.WriteLine($"p2 Hash :{p2.GetHashCode()}");
+
+
+    }
+
+    struct point
+    {
+
+        int x;
+        int y;
+
+        public point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+}
